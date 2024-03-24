cfa7269f7cf14ae2273121b07484038218d539c1
sesiliafebii <febifebbbb@gmail.com>
2024-03-24 11:27:01 +0700

    Modul 1 : Kak Akmal

---
+++ modul-1/230441100040-SesiliFebiDiyantika/manusia/Manusia.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package manusia;
+
+/**
+ *
+ * @author Sesilia Febi
+ */
+class  {
+    String Nama;
+    String Umur;
+    String Alamat;
+    
+    void berjalan() {
+        System.out.println(Nama + "Sedang Berjalan");
+    }
+}   
+    void berlari() {
+        System.out.println(Nama + "Sedang Berlari");
+    }
+
+    public class Manusia(){
+        public static void main(String[] args) {
+            System.out.println(Nama    + "sesil");
+            System.out.println(Umur    + "19 tahun");
+            System.out.println(Alamat  + "Lamongan");
+    
+    }
+
+    
+//    public static void main(String[] args) {
+//        Manusia obj = new Manusia();
+//        obj.berjalan();
+//        obj.berlari();
+    }
+}
